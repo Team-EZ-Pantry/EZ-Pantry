b48fdd4f00a656f17589745f7c23a2a87e7cae6d
Carguy10 <joseph.stephenson101@gmail.com>
2025-10-10 15:08:41 -0500

    import pantry_item
    Added pantry item import to pantry page

--- lib/screens/pantry_page.dart
+++ lib/screens/pantry_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:provider/provider.dart';
-
+import '../widgets/pantry_item.dart';
 import '../providers/pantry_provider.dart';
 import '../widgets/add_item.dart';
 import 'add-item_page.dart';
