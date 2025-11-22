486cd74428146d7c9b675afcfa0b58336a029fe4
noah-charlot <noah.charlot@students.pcci.edu>
2025-11-22 15:11:38 -0600

    Starting shopping page structure

--- lib/screens/shopping_page.dart
+++ lib/screens/shopping_page.dart
@@ -1,15 +1,31 @@
+
+// Core Packages
 import 'package:flutter/material.dart';
 
-class ShoppingPage extends StatelessWidget {
+
+class ShoppingPage extends StatefulWidget {
   const ShoppingPage({super.key});
 
   @override
+  State<ShoppingPage> createState() => _ShoppingPageState();
+}
+
+class _ShoppingPageState extends State<ShoppingPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Under construction. Come back soon!',
-        style: TextStyle(fontSize: 24),
-      ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        )),
     );
   }
+
 }
