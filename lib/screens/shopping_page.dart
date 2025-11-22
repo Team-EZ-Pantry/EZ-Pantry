4599a17417d5827de5eae7a30e5066d27a389be8
noah-charlot <noah.charlot@students.pcci.edu>
2025-11-22 16:46:09 -0600

    Classes and States for sorting button

--- lib/screens/shopping_page.dart
+++ lib/screens/shopping_page.dart
@@ -16,16 +16,67 @@ class _ShoppingPageState extends State<ShoppingPage> {
     super.initState();
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            
+            /// Sorting and top of page options
+            Row(
+              children: <Widget>[
+                 SortButton(),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox()
+              ],)
           ],
         )),
     );
   }
 
 }
+
+/// States for sort button
+class SortState<T>{
+  SortState({required this.display, required this.value});
+
+  final String display;
+  final T value;
+}
+
+final List<SortState<int>> sortingStates = [
+    SortState<int>(display: 'Name', value: 0),
+    SortState<int>(display: 'Created Date', value: 1),
+];
+
+class SortButton extends StatefulWidget {
+  const SortButton({super.key});
+
+  @override
+  SortButtonState createState() => SortButtonState();
+}
+
+class SortButtonState extends State<SortButton>{
+  int currentIndex = 0;
+
+  void nextState(){
+    setState(() {
+      // Modulus makes it loop around list
+      currentIndex = (currentIndex + 1) % sortingStates.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final SortState<int> currentSort = sortingStates[currentIndex];
+
+    return TextButton(
+      onPressed: nextState,
+      child: Text('Sort by ${currentSort.display}')
+    );
+  }
+}     
